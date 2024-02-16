5c1a6930b91fd25a83e9c4db9a5a5c20ebebf3b2
gp508 <114514250+gp508@users.noreply.github.com>
2024-02-16 14:52:27 +0000

    Adapting tutorial files ad hoc

--- EcoExe/EcoExe/views.py
+++ EcoExe/EcoExe/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404,render
 from django.template import loader
-from .models import Question
 # Create your views here.
 from django.http import HttpResponse
 from django.http import Http404
