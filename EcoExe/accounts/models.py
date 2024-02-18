120d0064d0c32bd1d0c8d3e28e57a5d794ab4d72
DanHarkness-Moore <djharknessmoore@hotmail.co.uk>
2024-02-18 13:15:35 +0000

    Connected database to Django

--- EcoExe/accounts/models.py
+++ EcoExe/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=16)
+    birthday = models.DateField()
+    def __str__(self):
+        return self.username
--- EcoExe/settings.py
+++ EcoExe/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
