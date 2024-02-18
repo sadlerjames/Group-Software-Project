3244900ef0ee8cb4e24afd96fa042b90a64b3583
Arrows <114746967+SamArrows@users.noreply.github.com>
2024-02-18 12:17:06 +0000

    Basic CSS and functional navbar
    - need to upload files in several uploads as the folders required for CSS have 100+ files

---
+++ EcoExe/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for EcoExe project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', views.loginpage, name="loginpage"),
+    path('accounts/signup/', SignUp.as_view(), name='signup'),
+    path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
+    path('profile/', views.profile, name="profile"),
+    path('home/', views.home, name="home")
+]
