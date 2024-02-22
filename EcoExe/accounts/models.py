e27b3d3140e40477399849106c64f43d76f5ffc8
jack-hales <124181833+jack-hales@users.noreply.github.com>
2024-02-22 10:46:20 +0000

    Added points field to database

--- EcoExe/accounts/models.py
+++ EcoExe/accounts/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=16)
     birthday = models.DateField()
+    points = models.IntegerField(default=0)
     def __str__(self):
         return self.username
