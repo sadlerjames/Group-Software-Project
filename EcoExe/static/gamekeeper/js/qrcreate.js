83cbc0259612950468958332053fbb4b8e70a717
George Piper <gp508@exeter.ac.uk>
2024-03-09 19:13:41 +0000

    Create dropdown showing different divs
    Co-authored-by: James Sadler <28812304+sadlerjames@users.noreply.github.com>

--- EcoExe/static/gamekeeper/js/qrcreate.js
+++ EcoExe/static/gamekeeper/js/qrcreate.js
@@ -1,30 +1,40 @@
-$(document).ready(function() {
-    let form_count = Number($("[name=extra_field_count]").val());
+// $(document).ready(function() {
+//     let form_count = Number($("[name=extra_field_count]").val());
 
-    //event.preventDefault(); // Prevent default form submission behavior
-    form_count ++;
+//     //event.preventDefault(); // Prevent default form submission behavior
+//     form_count ++;
     
-    //add a dropdown for what activity for the qr to link to
-    let dropDown = $('<p1>Activity for the code to link to:  </p1><select form="create-qr" id="sel"><option value="quiz">Quiz</option></select>')
-    form_count ++;
-    dropDown.attr('name','extra_field_'+form_count);
-    $("#forms").append(dropDown);
-    $("[name=extra_field_count]").val(form_count);
-    //we then need to display more form elements depending on what option is selected
+//     //add a dropdown for what activity for the qr to link to
+//     let dropDown = $('<p1>Activity for the code to link to:  </p1><select form="create-qr" id="sel"><option value="quiz">Quiz</option></select>')
+//     form_count ++;
+//     dropDown.attr('name','extra_field_'+form_count);
+//     $("#forms").append(dropDown);
+//     $("[name=extra_field_count]").val(form_count);
+//     //we then need to display more form elements depending on what option is selected
 
-    $("#sel").change(function(){
-        if(this.value=="quiz"){
-            /*
-            let dropDown = $('<p1>Quiz for the code to link to:  </p1><select form="create-qr" id="quiz_sel"></select>')
-            form_count ++;
-            dropDown.attr('name','extra_field_'+form_count);
-            $("#forms").append(dropDown);
-            $("[name=extra_field_count]").val(form_count);
-            //we then need to display more form elements depending on what option is selected
-            */
-        }
-    });
-    
-
-
+//     /*$("#sel").change(function(){
+//         if(this.value=="quiz"){
+//             console.log("Quiz selected");
+//             let dropDown = $('<p1>Quiz for the code to link to:  </p1><select form="create-qr" id="quiz_sel"></select>')
+//             form_count ++;
+//             dropDown.attr('name','extra_field_'+form_count);
+//             $("#forms").append(dropDown);
+//             $("[name=extra_field_count]").val(form_count);
+//             //we then need to display more form elements depending on what option is selected
+            
+//         }
+//     });*/
+// });
+$(document).ready(function(){
+    $("select").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".option").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".option").hide();
+            }
+        });
+    }).change();
 });
