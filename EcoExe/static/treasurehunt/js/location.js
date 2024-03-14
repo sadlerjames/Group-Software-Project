2de2db594e296475ee42bd5961c26278f436cd0d
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2024-03-14 21:42:58 +0000

    Reorganised js file + fixed post request in gamekeeper
    Co-authored-by: gp508 <gp508@users.noreply.github.com>

--- EcoExe/static/treasurehunt/js/location.js
+++ EcoExe/static/treasurehunt/js/location.js
@@ -1,5 +1,24 @@
 const x = document.getElementById("demo");
 
+
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Check if this cookie starts with the name we're looking for
+      if (cookie.startsWith(name + '=')) {
+          // Return the cookie value
+          return decodeURIComponent(cookie.substring(name.length + 1));
+      }
+  }
+  // Return null if the cookie with the specified name is not found
+  return null;
+}
+
+const csrfToken = getCookie('csrftoken');
+console.log(csrfToken);
+
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -13,9 +32,17 @@ function showPosition(position) {
   var long  = position.coords.latitude;
   var lat = position.coords.longitude;
   console.log(long);
-  fetch("/treasurehunt/validate", {
-    method: "POST",
-    headers: {},
-    body: JSON.stringify({ longitude:long, latitude:lat})
+
+  fetch("/treasurehunt/validate/", {
+    method: "POST", 
+    headers: {
+      "X-CSRFToken": csrfToken,
+      "Content-Type": "application/json" 
+    },
+    body: JSON.stringify({ 
+      longitude:long, 
+      latitude:lat})
+  }).catch(error => {
+      console.error('Error:', error);
   });
 }
--- EcoExe/treasurehunt/views.py
+++ EcoExe/treasurehunt/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def scan(request):
     return render(request, "scan.html")
 
 def validate(request):
+    print("HELLO")
+
+    print(request.body)
+
     if request.method == 'POST':
-        print(request.POST)
         return render(request,"scan.html")
