e6ec30209513fab7283625a27c4d61ec72daceb6
SamArrows <114746967+SamArrows@users.noreply.github.com>
2024-03-18 16:50:12 +0000

    Has game features, needs timer and linking to the main database and the ability to be integrated as a treasure hunt

--- EcoExe/static/sortit/js/sortit.js
+++ EcoExe/static/sortit/js/sortit.js
@@ -8,16 +8,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; 
 } 
 
+function areElementsTouching(element1, element2) {
+  let rect1 = element1.getBoundingClientRect();
+  let rect2 = element2.getBoundingClientRect();
+
+  return !(
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom ||
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right
+  );
+}
+
+var points = 0;
 var images = Array.from(document.querySelectorAll('.trash'));
+const bin = document.getElementById('bin');
+const bannerText = document.getElementById('banner-info');
 
 document.addEventListener('mousemove', (e) => {
-  const object = document.getElementById('bin');
-  object.style.left = e.offsetX + 'px';
+  bin.style.left = e.offsetX + 'px';
 });
 
 document.addEventListener('touchstart', function(e) {
-  const object = document.getElementById('bin');
-  initialX = e.touches[0].clientX - object.offsetLeft;
+  initialX = e.touches[0].clientX - bin.offsetLeft;
 });
 
 document.addEventListener('touchmove', function(e) {
@@ -26,8 +39,7 @@ document.addEventListener('touchmove', function(e) {
   currentX = e.touches[0].clientX - initialX;
 
   // Update the position of the DOM object
-  const object = document.getElementById('bin');
-  object.style.left = currentX + 'px';
+  bin.style.left = currentX + 'px';
   
 }, { passive: false });
 
@@ -52,7 +64,22 @@ function moveObjects() {
         image.style.left = newX + 'vw' //randomly set the x position
     }
     else{
+
         pos += (getRandomInt(1,10)) / 2.5; // Adjust this value to control the speed of the movement
+        if(areElementsTouching(image, bin)){
+            console.log("CAUGHT!");
+            if(points == 0){
+              points += 10;
+            }
+            else{
+              points = Math.floor(points * 1.1);
+            }
+            
+            bannerText.textContent = "Catch the rubbish before it falls in the sea! Points: " + points;
+            pos = 0; //reset image's position
+            var newX = getRandomInt(20, 90); 
+            image.style.left = newX + 'vw' //randomly set the x position
+        }
     }
     image.style.top = pos + 'vh';
   });
