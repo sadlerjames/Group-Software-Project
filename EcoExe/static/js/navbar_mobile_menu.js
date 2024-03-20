7322e13a27bddd2f999b82bd736e68e1d7027025
jack-hales <124181833+jack-hales@users.noreply.github.com>
2024-03-20 19:21:44 +0000

    Update navbar_mobile_menu.js

--- EcoExe/static/js/navbar_mobile_menu.js
+++ EcoExe/static/js/navbar_mobile_menu.js
@@ -1,5 +1,5 @@
 //this code effectively shows the menu for mobile view if a mobile is being used to access the webpage, otherwise the usual desktop view shows
-        //the menu simply has the same features but as a dropdown menu
+//the menu simply has the same features but as a dropdown menu
 $(document).ready(function() {
     var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
     var mobileMenu = document.querySelector(".mobile-menu");
