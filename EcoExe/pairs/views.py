aa97950e257d7c69858a934f31ab476e461f33f1
jack-hales <124181833+jack-hales@users.noreply.github.com>
2024-03-20 19:10:10 +0000

    Pairs Comment Check

--- EcoExe/pairs/views.py
+++ EcoExe/pairs/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from treasurehunt.views import activityFinished
 
+# Create view for playing the pairs game
 def play_game(request):
+    # Play game
     if request.method == 'GET':
         hunt = request.GET.get('hunt')
         return render(request, 'pairs/game.html',{'hunt':hunt})
+    # Game complete so show score
     else:
         score = int(request.POST.get('score'))
         if(score>2000):
@@ -12,5 +15,6 @@ def play_game(request):
         else:
             return render(request,"/treasurehunt/fail.html")
 
+# Create view for game image asset sources
 def view_sources(request):
     return render(request, 'pairs/sources.html')
