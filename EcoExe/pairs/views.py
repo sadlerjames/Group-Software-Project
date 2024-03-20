b62fbb699ac8ba5fbbd0ae9bcc8719630ed8b446
George Piper <gp508@exeter.ac.uk>
2024-03-20 10:30:29 +0000

    Added information for creating activities and treasure hunt

--- EcoExe/pairs/views.py
+++ EcoExe/pairs/views.py
@@ -4,10 +4,10 @@ from treasurehunt.views import activityFinished
 def play_game(request):
     if request.method == 'GET':
         hunt = request.GET.get('hunt')
-        return render(request, 'pairs/game.html',{'hunt':hunt})
+        return render(request, 'pairs/game.html',{'hunt':hunt}) #load the game page
     else:
         score = int(request.POST.get('score'))
-        if(score>2000):
+        if(score>2000): #if the user has passed
             return activityFinished(request,score/4000)
         else:
             return render(request,"/treasurehunt/fail.html")
