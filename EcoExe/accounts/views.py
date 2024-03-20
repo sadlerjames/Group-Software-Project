be004e3cf303d251840857ecaf0c4ef2d4210bd1
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2024-03-20 15:57:45 +0000

    Added forgot user password and connect to SMTP server

--- EcoExe/accounts/views.py
+++ EcoExe/accounts/views.py
@@ -4,13 +4,23 @@ from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import LoginForm, SignUpForm
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 from .forms import CustomPasswordChangeForm, UpdateUserForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
 from treasurehunt.treasure import Treasure
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'password_reset.html'
+    email_template_name = 'password_reset_email.html'
+    subject_template_name = 'password_reset_subject.html'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('dashboard')
 
 class SignUp(generic.CreateView):
     form_class = SignUpForm
@@ -114,4 +124,8 @@ def delete_account(request):
 
 def delete_account_success(request):
     return render(request, 'registration/delete_account_success.html')
+
+def password_reset_done(request):
+    return render(request, 'password_reset_complete.html')
+
 
